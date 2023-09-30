e78c8fd96d2b96d52c4f3a49ca6a8a0618fb70c7
Peyton Boggs <peytonrva@gmail.com>
2023-09-30 17:16:14 -0400

    Added front-end visual board as well as difficulty selection. The user is now asked to choose between, easy, medium, and hard, and invalid responses are caught. Then, a board is output with the correct visual specifications, though all future pieces are represented by dummy "x" characters.

--- src/edu/wm/cs/cs301/memorygame/LatinAlphabet.java
+++ src/edu/wm/cs/cs301/memorygame/LatinAlphabet.java
@@ -3,7 +3,7 @@ package edu.wm.cs.cs301.memorygame;
 public class LatinAlphabet implements Alphabet {
 	private char[] latinAlpha = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g'};
 	
-	public char[] toCharArray() {n
+	public char[] toCharArray() {
 		return latinAlpha;
 	}
 }
