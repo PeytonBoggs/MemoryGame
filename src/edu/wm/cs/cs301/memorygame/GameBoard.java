fc0091dd9cedb4f3b221b388f83412177bdbc181
Peyton Boggs <peytonrva@gmail.com>
2023-10-04 15:30:29 -0400

    Symbol Implementation
    These changes give the CharacterGamePiece their correct symbols when
    displaying the board. Based on the game's difficulty, the full list of
    symbols is truncated, doubled in length to create pairs, and shuffled.
    Then, when creating the board, each character in the list is inserted
    once. LatinAlphabet was also changed so that the symbols are have been
    changed from lower to upper case to better fit the specification.

--- src/edu/wm/cs/cs301/memorygame/GameBoard.java
+++ src/edu/wm/cs/cs301/memorygame/GameBoard.java
@@ -1,16 +1,41 @@
 package edu.wm.cs.cs301.memorygame;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GameBoard {
     private final GamePiece[][] board;
-    private char[] alphabet;
   
 	public GameBoard(int rows, int cols, Alphabet a) {
 		GamePiece[][] tempBoard = new GamePiece[rows][cols];
-		alphabet = a.toCharArray();
-    	
+		char[] fullAlphabet = a.toCharArray();
+
+		int numCharacters;
+		if (rows == 3) {
+			numCharacters = 6;
+		} else if (rows == 4) {
+			numCharacters = 14;
+		} else {
+			numCharacters = 28;
+		}
+		
+		char[] alphabet = new char[numCharacters];
+		System.arraycopy(fullAlphabet, 0, alphabet, 0, numCharacters);
+		
+		List<Character> symbolList = new ArrayList<Character> ();
+		for (char c : alphabet) {
+			symbolList.add(c);
+		}
+		
+		symbolList.addAll(symbolList);
+		Collections.shuffle(symbolList);
+		
+    	int symbolIndex = 0;
     	for(int i = 0; i < tempBoard.length; i++) {
     		for (int j = 0; j < tempBoard[i].length; j++) {
-    			GamePiece tempGamePiece = new CharacterGamePiece('x');
+    			GamePiece tempGamePiece = new CharacterGamePiece(symbolList.get(symbolIndex));
+    			symbolIndex++;
     			tempBoard[i][j] = tempGamePiece;
     		}
     	}
--- src/edu/wm/cs/cs301/memorygame/LatinAlphabet.java
+++ src/edu/wm/cs/cs301/memorygame/LatinAlphabet.java
@@ -4,8 +4,8 @@ public class LatinAlphabet implements Alphabet {
 	private char[] latinAlphabet;
 	
 	public LatinAlphabet() {
-		this.latinAlphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 
-				'B'};
+		this.latinAlphabet = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 
+				'b'};
 	}
 	
 	public char[] toCharArray() {
