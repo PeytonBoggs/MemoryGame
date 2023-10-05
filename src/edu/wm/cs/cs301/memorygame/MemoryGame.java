33991231fd3f85f7f1c22cae68d287f67113fcb2
Peyton Boggs <peytonrva@gmail.com>
2023-10-04 21:03:54 -0400

    getRC Method Implementation
    Method named getRC implemented that takes, validates, and returns a
    user's input for row and column selection. A valid input is one in form
    "R C" or "quit", where R and C are ints in the range of the rows and
    columns on the board. If anything is invalid - the string is not in that
    form or the row and column inputs are out of range - an exception is
    thrown and the user is prompted again. Other methods in MemoryGame.java
    have been created to simplify the MemoryGame method, nothing new has
    been added to them, they've just been copy-pasted into their own
    methods.

--- src/edu/wm/cs/cs301/memorygame/MemoryGame.java
+++ src/edu/wm/cs/cs301/memorygame/MemoryGame.java
@@ -7,6 +7,36 @@ public class MemoryGame {
 	Scanner s = new Scanner(System.in);
 	
 	public MemoryGame() {	
+		String difficulty = getDifficulty();
+		String alphabet = getAlphabet();
+		
+		GameBoard board = createBoard(difficulty, alphabet);
+		GamePiece[][] gameBoard = board.getBoard();
+		
+		int turn = 1;
+		boolean finished = false;
+		
+		while(!finished) {
+			System.out.println("Turn: " + turn);
+			printBoard(gameBoard);
+			
+			String firstCardRC = getRC(gameBoard);
+			gameBoard[Integer.valueOf(firstCardRC.substring(0, 1)) - 1][Integer.valueOf(firstCardRC.substring(2, 3)) - 1].setVisible(true);
+			
+			System.out.println("Turn: " + turn);
+			printBoard(gameBoard);
+			
+			String secondCardRC = getRC(gameBoard);
+			gameBoard[Integer.valueOf(secondCardRC.substring(0, 1)) - 1][Integer.valueOf(secondCardRC.substring(2, 3)) - 1].setVisible(true);
+			
+			System.out.println("Turn: " + turn);
+			printBoard(gameBoard);
+			
+			finished = true;
+		}
+	}
+	
+	public String getDifficulty() {
 		String selectedDifficulty = "";
 		boolean badInput = true;
 		while(badInput) {
@@ -18,9 +48,12 @@ public class MemoryGame {
 				System.out.println("Invalid difficulty.");
 			}
 		}
-		
+		return selectedDifficulty;
+	}
+	
+	public String getAlphabet() {
 		String selectedAlphabet = "";
-		badInput = true;
+		boolean badInput = true;
 		while (badInput) {
 			System.out.print("Select symbol set: 'latin' or 'greek': ");
 			selectedAlphabet = s.nextLine().toLowerCase();
@@ -30,40 +63,62 @@ public class MemoryGame {
 				System.out.println("Invalid symbol set.");
 			}
 		}
-		
-		if (selectedAlphabet.equals("latin")) {
+		return selectedAlphabet;
+	}
+	
+	public GameBoard createBoard(String difficulty, String alphabet) {
+		if (alphabet.equals("latin")) {
 			this.a = new LatinAlphabet();
-			if (selectedDifficulty.equals("easy")) {
+			if (difficulty.equals("easy")) {
 				board = new GameBoard(3, 4, a);
-			} else if (selectedDifficulty.equals("medium")) {
+			} else if (difficulty.equals("medium")) {
 				board = new GameBoard(4, 7, a);
 			} else {
 				board = new GameBoard(7, 8, a);
 			}
 		} else {
 			this.a = new GreekAlphabet();
-			if (selectedDifficulty.equals("easy")) {
+			if (difficulty.equals("easy")) {
 				board = new GameBoard(3, 4, a);
-			} else if (selectedDifficulty.equals("medium")) {
+			} else if (difficulty.equals("medium")) {
 				board = new GameBoard(4, 7, a);
 			} else {
 				board = new GameBoard(7, 8, a);
 			}
 		}
+		return board;
+	}
 	
-		GamePiece[][] gameBoard = board.getBoard();
+	public String getRC(GamePiece[][] gameBoard) {
+		int upperRowBound = gameBoard.length;
+		int upperColBound = gameBoard[0].length;
 		
-		int turn = 1;
-		boolean finished = false;
-		
-		while(!finished) {
-			System.out.println("Turn: " + turn);
-			printBoard(gameBoard);
+		boolean badInput = true;
+		while(badInput) {
 			System.out.print("Choose a tile [R C] or type 'quit' to exit: ");
-			String firstInput = s.nextLine();
+			String input = s.nextLine().toLowerCase();
 			
-			finished = true;
+			try {
+				if (input.equals("quit")) {
+					return input;
+				}
+				
+				if (input.length() != 3) {
+					throw new Exception();
+				}
+				
+				Integer r = Integer.valueOf(input.substring(0, 1));
+				char middle = input.charAt(1);
+				Integer c = Integer.valueOf(input.substring(2, 3));
+
+				if (1 <= r && r <= upperRowBound && middle == ' ' && 1 <= c && c <= upperColBound) {
+					return input;
+				}
+			} catch (Exception e) {
+				System.out.println("Invalid input.");
+			}
 		}
+		return "";
 	}
 	
 	public void printBoard(GamePiece[][] gameBoard) {
