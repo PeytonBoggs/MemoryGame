77b72b82b8251ef54af8a24a71d5cc4bcf44cafb
Peyton Boggs <peytonrva@gmail.com>
2023-10-11 16:20:29 -0400

    Play Again Option
    This update adds a getPlayAgain method that returns a boolean - true if
    the user inputs yes, and false if no. The method also validates the
    user's input and will ask again if invalid. The logic and method calls
    that the game is played through and was once in MemoryGame is now in its
    own playGame method. This method is called by MemoryGame and can call
    itself if playAgain is true. This way, the user can play again without
    being shown the welcome message each time. Comments have also been added
    with a short description of each method in MemoryGame.java.

--- src/edu/wm/cs/cs301/memorygame/MemoryGame.java
+++ src/edu/wm/cs/cs301/memorygame/MemoryGame.java
@@ -10,10 +10,16 @@ public class MemoryGame {
 	private Alphabet a;
 	Scanner s = new Scanner(System.in);
 		
-	public MemoryGame() {		
+	//Calls to get welcome message and starts a new game
+	public MemoryGame() {	
 		printWelcomeMessage();
 		waitForEnter();
 		
+		playGame();
+	}
+	
+	//Plays one instance of a game
+	public void playGame() {
 		try {
 			printLeaderboard();
 		} catch (IOException e) {
@@ -28,9 +34,9 @@ public class MemoryGame {
 		GamePiece[][] gameBoard = board.getBoard();
 		
 		int turn = 1;
-		boolean finished = false;
+		boolean quit = false;
 		
-		while(!finished) {
+		while(!quit) {
 			System.out.println("Turn: " + turn);
 			printBoard(gameBoard);
 			
@@ -38,6 +44,7 @@ public class MemoryGame {
 			firstCard.setVisible(true);
 			if (firstCard.getSymbol() == 'q') {
 				System.out.println("Game aborted.");
+				quit = true;
 				break;
 			}
 			
@@ -48,6 +55,7 @@ public class MemoryGame {
 			secondCard.setVisible(true);
 			if (secondCard.getSymbol() == 'q') {
 				System.out.println("Game aborted.");
+				quit = true;
 				break;
 			}
 			
@@ -74,14 +82,198 @@ public class MemoryGame {
 		System.out.println("Revealed Board:");
 		revealBoard(gameBoard);
 		
-		try {
-			updateLeaderboard(difficulty, turn);
-		} catch (IOException e) {
-			System.out.println("IOException caught.");
-			e.printStackTrace();
+		if (!quit) {
+			try {
+				updateLeaderboard(difficulty, turn);
+			} catch (IOException e) {
+				System.out.println("IOException caught.");
+				e.printStackTrace();
+			}	
+		}
+		
+		boolean playAgain = getPlayAgain();
+		if (playAgain) {
+			playGame();
 		}
 	}
 	
+	//Prints a welcome message to console
+	public void printWelcomeMessage() {
+		System.out.println("Welcome to Memory Game!");
+		System.out.println("In this game, you'll be shown a grid of cards that are initially flipped over.");
+		System.out.println("There will be numbers along the grid that indicate which row and column a card is on.");
+		System.out.println("Each turn, you can select two cards to be flipped over using the grid notation [row column].");
+		System.out.println("If the two cards match, they will stay revealed - if not, they will be flipped back over.");
+		System.out.println("The object is to reveal all cards in as few turns as possible. Good luck!");
+	}
+	
+	//Waits for the user to hit enter to continue
+	public void waitForEnter() {
+		System.out.print("Press <ENTER> to continue: ");
+		@SuppressWarnings("unused")
+		String input = s.nextLine().toLowerCase();
+		System.out.println("");
+	}
+	
+	//Returns a valid input for the game's difficulty
+	public String getDifficulty() {
+		String selectedDifficulty = "";
+		boolean badInput = true;
+		while(badInput) {
+			System.out.print("Select difficulty: 'easy', 'medium', or 'hard': ");
+			selectedDifficulty = s.nextLine().toLowerCase();
+			if (selectedDifficulty.equals("easy") || selectedDifficulty.equals("medium") || selectedDifficulty.equals("hard")) {
+				badInput = false;
+			} else {
+				System.out.println("Invalid difficulty.");
+			}
+		}
+		return selectedDifficulty;
+	}
+	
+	//Returns a valid input for the game's alphabet
+	public String getAlphabet() {
+		String selectedAlphabet = "";
+		boolean badInput = true;
+		while (badInput) {
+			System.out.print("Select symbol set: 'latin' or 'greek': ");
+			selectedAlphabet = s.nextLine().toLowerCase();
+			if (selectedAlphabet.equals("latin") || selectedAlphabet.equals("greek")) {
+				badInput = false;
+			} else {
+				System.out.println("Invalid symbol set.");
+			}
+		}
+		return selectedAlphabet;
+	}
+	
+	//Returns a new board with selected difficulty and alphabet
+	public GameBoard createBoard(String difficulty, String alphabet) {
+		if (alphabet.equals("latin")) {
+			this.a = new LatinAlphabet();
+			if (difficulty.equals("easy")) {
+				board = new GameBoard(3, 4, a);
+			} else if (difficulty.equals("medium")) {
+				board = new GameBoard(4, 7, a);
+			} else {
+				board = new GameBoard(7, 8, a);
+			}
+		} else {
+			this.a = new GreekAlphabet();
+			if (difficulty.equals("easy")) {
+				board = new GameBoard(3, 4, a);
+			} else if (difficulty.equals("medium")) {
+				board = new GameBoard(4, 7, a);
+			} else {
+				board = new GameBoard(7, 8, a);
+			}
+		}
+		return board;
+	}
+	
+	//Prints the current state of the board
+	public void printBoard(GamePiece[][] gameBoard) {
+		System.out.print("     ");
+		for (int i = 0; i < gameBoard[0].length; i++) {
+			System.out.print((i+1) + "   ");
+		}
+		System.out.println();
+		
+		System.out.print("   =");
+		for (int i = 0; i < gameBoard[0].length; i++) {
+			System.out.print("====");
+		}
+		System.out.println("");
+		
+		for (int i = 0; i < gameBoard.length; i++) {
+			System.out.print(i+1 + " ||");
+			for (int j = 0; j < gameBoard[i].length; j++) {
+				System.out.print(" " + gameBoard[i][j].getSymbol() + " |");
+			}
+			System.out.println("|");
+			if (i+1 != gameBoard.length) {
+				System.out.print("  --");
+				for (int j = 0; j < gameBoard[i].length; j++) {
+					System.out.print("----");
+				}
+				System.out.println("-");
+			}
+		}
+		
+		System.out.print("   =");
+		for (int i = 0; i < gameBoard[0].length; i++) {
+			System.out.print("====");
+		}
+		System.out.println();
+	}
+	
+	//Validates the user's input and returns the valid selected game piece
+	public GamePiece getCard(GamePiece[][] gameBoard) {
+		int upperRowBound = gameBoard.length;
+		int upperColBound = gameBoard[0].length;
+		
+		GamePiece quit = new CharacterGamePiece('q');
+		
+		boolean badInput = true;
+		while(badInput) {
+			System.out.print("Choose a tile [R C] or type 'quit' to exit: ");
+			String input = s.nextLine().toLowerCase();
+			
+			try {
+				if (input.equals("quit")) {
+					return quit;
+				}
+				
+				if (input.length() != 3) {
+					throw new Exception();
+				}
+				
+				Integer r = Integer.valueOf(input.substring(0, 1));
+				char middle = input.charAt(1);
+				Integer c = Integer.valueOf(input.substring(2, 3));
+				
+				if (1 <= r && r <= upperRowBound && middle == ' ' && 1 <= c && c <= upperColBound) {
+					GamePiece inputCard = gameBoard[Integer.valueOf(input.substring(0, 1)) - 1][Integer.valueOf(input.substring(2, 3)) - 1];
+					if (inputCard.isVisible()) {
+						throw new Exception();
+					}
+					return inputCard;
+				} else {
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.println("Invalid input.");
+			}
+		}
+		return quit;
+	}
+	
+	//Returns true if the game is won, false if not
+	public boolean checkWon(GamePiece[][] gameBoard) {
+		boolean won = true;
+		
+		for (int i = 0; i < gameBoard.length; i++) {
+			for (int j = 0; j < gameBoard[0].length; j++) {
+				if (!gameBoard[i][j].isVisible()) {
+					won = false;
+				}
+			}
+		}
+		
+		return won;
+	}
+	
+	//Makes all game pieces visible and prints board
+	public void revealBoard(GamePiece[][] gameBoard) {
+		for (int i = 0; i < gameBoard.length; i++) {
+			for (int j = 0; j < gameBoard[i].length; j++) {
+				gameBoard[i][j].setVisible(true);	
+			}
+		}
+		printBoard(gameBoard);
+	}
+	
+	//Returns an array of data from the leaderboard
 	public String[] getLeaderboard() throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("resources/leaderboard.txt"));
 		String[] toReturn = new String[6];
@@ -117,10 +309,10 @@ public class MemoryGame {
 		return toReturn;
 	}
 	
+	//Prints the leaderboard to console
 	public void printLeaderboard() throws IOException {		
 		String[] leaderboard = getLeaderboard();
-			
-		System.out.println();
+		
 		System.out.println("Current Leaderboard:");
 		System.out.println("====================");
 		if (leaderboard[0] == null) {
@@ -141,6 +333,7 @@ public class MemoryGame {
 		System.out.println();
 	}
 	
+	//Updates the leaderboard if applicable
 	public void updateLeaderboard(String difficulty, int turn) throws IOException {	
 		String stringRecord = "-1";
 		String[] leaderboard = getLeaderboard();
@@ -196,170 +389,22 @@ public class MemoryGame {
 		fw.close();
 	}
 	
-	public void printWelcomeMessage() {
-		System.out.println("Welcome to Memory Game!");
-		System.out.println("In this game, you'll be shown a grid of cards that are initially flipped over.");
-		System.out.println("There will be numbers along the grid that indicate which row and column a card is on.");
-		System.out.println("Each turn, you can select two cards to be flipped over using the grid notation [row column].");
-		System.out.println("If the two cards match, they will stay revealed - if not, they will be flipped back over.");
-		System.out.println("The object is to reveal all cards in as few turns as possible. Good luck!");
-	}
-	
-	public String getDifficulty() {
-		String selectedDifficulty = "";
-		boolean badInput = true;
-		while(badInput) {
-			System.out.print("Select difficulty: 'easy', 'medium', or 'hard': ");
-			selectedDifficulty = s.nextLine().toLowerCase();
-			if (selectedDifficulty.equals("easy") || selectedDifficulty.equals("medium") || selectedDifficulty.equals("hard")) {
-				badInput = false;
-			} else {
-				System.out.println("Invalid difficulty.");
-			}
-		}
-		return selectedDifficulty;
-	}
-	
-	public String getAlphabet() {
-		String selectedAlphabet = "";
+	//Validates a user's answer and returns true if yes, false if no
+	public boolean getPlayAgain() {
+		boolean playAgain = false;
 		boolean badInput = true;
 		while (badInput) {
-			System.out.print("Select symbol set: 'latin' or 'greek': ");
-			selectedAlphabet = s.nextLine().toLowerCase();
-			if (selectedAlphabet.equals("latin") || selectedAlphabet.equals("greek")) {
+			System.out.print("Play again? 'yes' or 'no':");
+			String input = s.nextLine().toLowerCase();
+			if (input.equals("yes")) {
+				playAgain = true;
+				badInput = false;
+			} else if (input.equals("no")) {
 				badInput = false;
 			} else {
-				System.out.println("Invalid symbol set.");
-			}
-		}
-		return selectedAlphabet;
-	}
-	
-	public GameBoard createBoard(String difficulty, String alphabet) {
-		if (alphabet.equals("latin")) {
-			this.a = new LatinAlphabet();
-			if (difficulty.equals("easy")) {
-				board = new GameBoard(3, 4, a);
-			} else if (difficulty.equals("medium")) {
-				board = new GameBoard(4, 7, a);
-			} else {
-				board = new GameBoard(7, 8, a);
-			}
-		} else {
-			this.a = new GreekAlphabet();
-			if (difficulty.equals("easy")) {
-				board = new GameBoard(3, 4, a);
-			} else if (difficulty.equals("medium")) {
-				board = new GameBoard(4, 7, a);
-			} else {
-				board = new GameBoard(7, 8, a);
-			}
-		}
-		return board;
-	}
-	
-	public GamePiece getCard(GamePiece[][] gameBoard) {
-		int upperRowBound = gameBoard.length;
-		int upperColBound = gameBoard[0].length;
-		
-		GamePiece quit = new CharacterGamePiece('q');
-		
-		boolean badInput = true;
-		while(badInput) {
-			System.out.print("Choose a tile [R C] or type 'quit' to exit: ");
-			String input = s.nextLine().toLowerCase();
-			
-			try {
-				if (input.equals("quit")) {
-					return quit;
-				}
-				
-				if (input.length() != 3) {
-					throw new Exception();
-				}
-				
-				Integer r = Integer.valueOf(input.substring(0, 1));
-				char middle = input.charAt(1);
-				Integer c = Integer.valueOf(input.substring(2, 3));
-				
-				if (1 <= r && r <= upperRowBound && middle == ' ' && 1 <= c && c <= upperColBound) {
-					GamePiece inputCard = gameBoard[Integer.valueOf(input.substring(0, 1)) - 1][Integer.valueOf(input.substring(2, 3)) - 1];
-					if (inputCard.isVisible()) {
-						throw new Exception();
-					}
-					return inputCard;
-				} else {
-					throw new Exception();
-				}
-			} catch (Exception e) {
 				System.out.println("Invalid input.");
 			}
 		}
-		return quit;
-	}
-	
-	public void waitForEnter() {
-		System.out.print("Press <ENTER> to continue: ");
-		@SuppressWarnings("unused")
-		String input = s.nextLine().toLowerCase();
-		System.out.println("");
-	}
-	
-	public boolean checkWon(GamePiece[][] gameBoard) {
-		boolean won = true;
-		
-		for (int i = 0; i < gameBoard.length; i++) {
-			for (int j = 0; j < gameBoard[0].length; j++) {
-				if (!gameBoard[i][j].isVisible()) {
-					won = false;
-				}
-			}
-		}
-		
-		return won;
-	}
-	
-	public void printBoard(GamePiece[][] gameBoard) {
-		System.out.print("     ");
-		for (int i = 0; i < gameBoard[0].length; i++) {
-			System.out.print((i+1) + "   ");
-		}
-		System.out.println();
-		
-		System.out.print("   =");
-		for (int i = 0; i < gameBoard[0].length; i++) {
-			System.out.print("====");
-		}
-		System.out.println("");
-		
-		for (int i = 0; i < gameBoard.length; i++) {
-			System.out.print(i+1 + " ||");
-			for (int j = 0; j < gameBoard[i].length; j++) {
-				System.out.print(" " + gameBoard[i][j].getSymbol() + " |");
-			}
-			System.out.println("|");
-			if (i+1 != gameBoard.length) {
-				System.out.print("  --");
-				for (int j = 0; j < gameBoard[i].length; j++) {
-					System.out.print("----");
-				}
-				System.out.println("-");
-			}
-		}
-		
-		System.out.print("   =");
-		for (int i = 0; i < gameBoard[0].length; i++) {
-			System.out.print("====");
-		}
-		System.out.println();
-	}
-	
-	public void revealBoard(GamePiece[][] gameBoard) {
-		for (int i = 0; i < gameBoard.length; i++) {
-			for (int j = 0; j < gameBoard[i].length; j++) {
-				gameBoard[i][j].setVisible(true);	
-			}
-		}
-		printBoard(gameBoard);
+		return playAgain;
 	}
 }
